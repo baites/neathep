f34f91075da970fb72a3f174d4e1fbb5e9b3edb7
Victor Eduardo Bazterra <baites@fnal.gov>
2012-08-07 15:27:12 -0500

    Adding library directory for compilin in scilinux.

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,6 @@
 # Installation script
 
+import sys;
 from distutils.core import setup, Extension
 
 setup(
@@ -8,7 +9,13 @@ setup(
       description='A NEAT (NeuroEvolution of Augmenting Topologies) implementation',
       packages=['src/neat', 'src/neat/iznn', 'src/neat/nn', 'src/neat/ctrnn', 'src/neat/ifnn'],
       ext_modules=[
-               Extension('src/neat/iznn/iznn_cpp', ['src/neat/iznn/iznn.cpp'], extra_compile_args=['-Wno-write-strings']),
-               Extension('src/neat/nn/ann', ['src/neat/nn/nn_cpp/ANN.cpp', 'src/neat/nn/nn_cpp/PyANN.cpp'], extra_compile_args=['-Wno-write-strings']),
-               Extension('src/neat/ifnn/ifnn_cpp', ['src/neat/ifnn/ifnn.cpp'], extra_compile_args=['-Wno-write-strings']),],
+               Extension('src/neat/iznn/iznn_cpp', ['src/neat/iznn/iznn.cpp'], 
+                   extra_compile_args=['-Wno-write-strings'],
+                   library_dirs=[sys.prefix + "/lib"]),
+               Extension('src/neat/nn/ann', ['src/neat/nn/nn_cpp/ANN.cpp', 'src/neat/nn/nn_cpp/PyANN.cpp'], 
+                   extra_compile_args=['-Wno-write-strings'],
+                   library_dirs=[sys.prefix + "/lib"]),
+               Extension('src/neat/ifnn/ifnn_cpp', ['src/neat/ifnn/ifnn.cpp'], 
+                   extra_compile_args=['-Wno-write-strings'],
+                   library_dirs=[sys.prefix + "/lib"]),],
 )
